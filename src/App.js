64cb58c8ed5f119c098a6d323d148cef4dbf05ad
Shahrom <mshahromali@gmail.com>
2024-01-30 05:10:56 -0500

    barebones prep

--- src/App.js
+++ src/App.js
@@ -2,155 +2,155 @@ import { useState, useEffect } from "react";
 import storage from "./firebaseConfiguration";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import WebFont from "webfontloader";
+import background from "./background.png";
 
 function App() {
-  // State to store uploaded file
-  const [file, setFile] = useState("");
-  // progress
-  const [percent, setPercent] = useState(0);
+  const fontStyle = "Kanit";
 
-  const fontStyle = "Chilanka";
+  const stylesheet = {
+    mainDiv: {
+      backgroundImage: `url(${background})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      fontFamily: fontStyle,
+      height: "150%",
+      width: "100%",
+      position: "absolute",
+    },
+    nostagainnetwork: {
+      paddingTop: "8%",
+      fontSize: 18,
+      textAlign: "center",
+      color: "#fff",
+      marginBottom: 0,
+    },
+    presents: {
+      fontFamily: "Ubuntu",
+      fontStyle: "italic",
+      color: "#fff",
+      fontSize: 12,
+      fontWeight: "lighter",
+      marginTop: 0,
+    },
+    title: {
+      color: "#ffffff",
+      fontSize: 60,
+      textAlign: "center",
+      marginBottom: 0,
+    },
+    subtitle: {
+      fontFamily: "Ubuntu",
+      fontStyle: "italic",
+      color: "#fff",
+      fontSize: 14,
+      fontWeight: "lighter",
+      marginTop: 0,
+      marginBottom: 50,
+    },
+  };
 
   useEffect(() => {
     WebFont.load({
       google: {
-        families: ["Droid Sans", "Chilanka", "Permanent Marker"],
+        families: ["Droid Sans", "Kanit", "Ubuntu"],
       },
     });
   }, []);
 
-  // Handle file upload event and update state
-  function handleChange(event) {
-    setFile(event.target.files[0]);
+  function handleSubmit() {
+    console.log("DEBUG::SUBMIT");
   }
-
-  const handleUpload = () => {
-    if (!file) {
-      alert("Please upload an image first!");
-      return;
-    }
-
-    const storageRef = ref(storage, `${file.name}`);
-
-    // progress can be paused and resumed. It also exposes progress updates.
-    // Receives the storage reference and the file to upload.
-    const uploadTask = uploadBytesResumable(storageRef, file);
-
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const percent = Math.round(
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        );
-
-        // update progress
-        setPercent(percent);
-      },
-      (err) => console.log(err),
-      () => {
-        // download url
-        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-          console.log(url);
-        });
-      }
-    );
-  };
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        fontFamily: fontStyle,
-        height: "100%",
-        width: "100%",
-        position: "absolute",
-        backgroundColor: "#E5DCC5",
-      }}
-    >
-      <h1
+    <div style={stylesheet.mainDiv}>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <h3 style={stylesheet.nostagainnetwork}>NOSTAGAIN</h3>
+        <h3 style={{ ...stylesheet.nostagainnetwork, color: "#787878" }}>
+          NETWORK
+        </h3>
+      </div>
+      <h5 style={stylesheet.presents}>presents</h5>
+
+      <h1 style={stylesheet.title}>TIME IN A BOTTLE</h1>
+      <h5 style={stylesheet.subtitle}>
+        Coming together, once again, to share our research and collectively
+        nostalgize.
+      </h5>
+
+      <div
         style={{
-          paddingTop: "8%",
-          paddingBottom: "5%",
-          fontSize: 38,
-          textAlign: "center",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyItems: "center",
+          backgroundColor: "#93939350",
+          borderRadius: 12,
+          padding: 20,
         }}
       >
-        LOSTAGAIN Symposium
-      </h1>
+        <p
+          style={{
+            fontFamily: "Ubuntu",
+            fontStyle: "italic",
+            color: "#fff",
+            fontSize: 14,
+            fontWeight: "lighter",
+          }}
+        >
+          What to share? You felt this too? <br />
+          This is the prompt for what we expect them to share. Provoke their
+          memories and stimulate <br /> their nost-centers for memories,
+          feelings, and mental images.
+        </p>
 
-      <text
-        style={{
-          textAlign: "center",
-          paddingBottom: "1%",
-          fontStyle: "italic",
-        }}
-      >
-        Objects of the past,
-      </text>
-
-      <text
-        style={{
-          textAlign: "center",
-          paddingBottom: "1%",
-          fontStyle: "italic",
-        }}
-      >
-        Taken from us too fast
-      </text>
-
-      <text
-        style={{
-          textAlign: "center",
-          paddingBottom: "3%",
-          fontStyle: "italic",
-        }}
-      >
-        Now found, now LostAgain 
-      </text>
-
-      <text style={{ textAlign: "center", paddingBottom: "4%" }}>
-        Something that makes you nostalgic? Share it with us on this wall!
-      </text>
-
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyItems: "center" }}>
         <input
-          type="file"
-          onChange={handleChange}
-          accept="/image/*"
-          style={{ fontFamily: fontStyle }}
+          type="text"
+          style={{
+            borderRadius: 10,
+            display: "flex",
+            width: "100%",
+            minHeight: 100,
+            fontFamily: fontStyle,
+            fontStyle: "italic",
+          }}
+          maxLength={140}
+          placeholder="Maximum 140 characters"
         />
         <button
           style={{
             fontSize: 20,
             fontFamily: fontStyle,
-            padding: 15,
             alignItems: "center",
             justifyItems: "center",
             marginTop: 20,
             borderRadius: 10,
             borderColor: "black",
-            width: "50%"
+            width: "50%",
+            height: "50%",
           }}
-          onClick={handleUpload}
+          onClick={handleSubmit}
         >
-          <text>Upload</text>
+          <text>Submit</text>
         </button>
-        <p style={{ textAlign: "center" }}>{percent}% done</p>
-
-        <text style={{ textAlign: "center", paddingTop: "15%", fontSize: 20 }}>
-          Project: Lost Again {""}
-          <a href="https://projectlostagain.wixsite.com/nostagain">
-            Website
-          </a>{" | "}
-          <a href="https://twitter.com/Nostagain">
-            Twitter
-          </a>{" | "}
-          <a href="https://www.youtube.com/@nostagain">
-            YouTube
-          </a>{" "}
-        </text>
       </div>
+      <text
+        style={{
+          textAlign: "center",
+          paddingTop: "15%",
+          fontSize: 20,
+          color: "#fff",
+        }}
+      >
+        NOSTAGAINNETWORK {""}
+        <a href="https://nostagain.ca"> Website</a>
+        {" | "}
+        <a href="https://twitter.com/Nostagain">X Page</a>
+        {" | "}
+        <a href="https://www.youtube.com/@nostagain">YouTube</a>
+      </text>
     </div>
   );
 }
